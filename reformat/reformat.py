676118486ee02a2f5bcc89bd31a62db7ec38256f
Behrouz Shamsaei <behrooz_sh62@yahoo.com>
2017-09-11 02:01:54 -0400

    second version, argparse added, a bug rectified.

--- reformat/reformat.py
+++ reformat/reformat.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import argparse
+
 class Reformat(object):
     """reformat class is to read the input file and other auxiliary files and reformat it in the
      desired format.
     """
-
 
     def __init__(self):
         self.motif_and_modification_list = []
@@ -17,21 +18,47 @@ class Reformat(object):
         self.gene_to_pathway_list = []
 
     def read_input_files(self):
-        with open('resource/Protein_iPSC_Metdata_Master_modified.xlsx', "r") as xls_data_file:
-            self._metadata = pd.read_excel(xls_data_file)
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-i", "--input", help='input file address')
+        parser.add_argument("-m", "--meta", help='resource file address for renaming samples')
+        parser.add_argument("-u", "--unimod", help='unimod file address')
+        parser.add_argument("-o", "--output", help='output file address')
+        parser.add_argument("-t", "--tab", help='input data sheet or tab in the xlsx file')
+        args = parser.parse_args()
 
-        with open('resource/Renaming_neuroLINCS.xlsx', "r") as xls_data_file:
-            self._metadata2 = pd.read_excel(xls_data_file, header = None)
+        self.input_address = 'input/outputmatrix_OpenSWATH.xlsx'
+        self.resource_address = 'resource/Renaming_neuroLINCS.xlsx'
+        self.unimod_address = 'resource/srep07896-s9.xls'
+        self.output_address = 'output/outputmatrix_OpenSWATH.csv'
+        self.tab = 'IPSC_DDA_6600_QE_combined_Canon'
+
+        if args.input:
+            self.input_address = args.input
+
+        if args.meta:
+            self.resource_address = args.meta
+
+        if args.unimod:
+            self.unimod_address = args.unimod
+
+        if args.output:
+            self.output_address = args.output
+
+        if args.tab:
+            self.tab = args.tab
+
+        with open(self.resource_address, "r") as xls_data_file:
+            self._metadata = pd.read_excel(xls_data_file, header = None)
 
         # This file is coming from www.nature.com/
         # Just search for srep07896-s9.xls in google
 
-        with open('resource/srep07896-s9.xls', "r") as xls_data_file:
-            self._uniModdata = pd.read_excel(xls_data_file)
+        with open(self.unimod_address, "r") as xls_data_file:
+            self._unimod_data = pd.read_excel(xls_data_file)
 
 
-        xls = pd.ExcelFile('input/outputmatrix_OpenSWATH.xlsx')
-        self._input_data = xls.parse('IPSC_DDA_6600_QE_combined_Canon')
+        xls = pd.ExcelFile(self.input_address)
+        self._input_data = xls.parse(self.tab)
 
         return
 
@@ -40,7 +67,6 @@ class Reformat(object):
         self._input_data = self._input_data[self._input_data['Peptide'].str.contains("DECOY") == False]
 
         return
-
 
     def replace_sample_intensity_column(self):
         # Get the header names
@@ -52,40 +78,25 @@ class Reformat(object):
 
         return
 
-    def find_new_sample_names(self, pre_sample_names):
-        """This function Replace sample 'intensity_' column headers with official sample generation identifiers
-            @param: pre_sample_names The column names of the input data
-            """
-        new_sample_names = {}
-        Intensity_MS_File_Name = self._metadata['Intensity_MS_File_Name']
-
-        IP_Center_Specific_ID = self._metadata['IP_Center_Specific_ID']
-        for sample in range (0, len(Intensity_MS_File_Name)):
-            for pre_sample in pre_sample_names:
-
-                stripped = str(Intensity_MS_File_Name[sample])[5:].strip()
-                if stripped in str(pre_sample):
-                    new_sample_names[str(pre_sample)] = str(IP_Center_Specific_ID[sample])
-
-        return new_sample_names
-
     def find_new_sample_names2(self, pre_sample_names):
         """This function Replace sample 'intensity_' column headers with official sample generation identifiers
             @param: pre_sample_names The column names of the input data
             """
         new_sample_names = {}
 
-        Intensity_MS_Sample_Name = self._metadata2.loc[0,:]
+        intensity_ms_sample_name = self._metadata.loc[0,:]
 
+        ip_center_specific_id = self._metadata.loc[1,:]
 
-        IP_Center_Specific_ID = self._metadata2.loc[1,:]
-
-        for sample in range (0, len(Intensity_MS_Sample_Name)):
+        for sample in range (0, len(intensity_ms_sample_name)):
             for pre_sample in pre_sample_names:
-                if str(Intensity_MS_Sample_Name[sample]) == str(pre_sample):
-                    new_sample_names[str(pre_sample)] = str(IP_Center_Specific_ID[sample])
+                if str(intensity_ms_sample_name[sample]) == str(pre_sample):
+                    new_sample_names[str(pre_sample)] = str(ip_center_specific_id[sample])
 
         return new_sample_names
+
+    def find_char(self, str, ch):
+        return [i for i, ltr in enumerate(str) if ltr == ch]
 
     def reformat_peptide_column(self):
         unimod_peptide = []
@@ -96,26 +107,51 @@ class Reformat(object):
         for peptide in peptide_col:
             splitted_by_underscore = str(peptide).split("_")
             pep = splitted_by_underscore[1]
+
             unimod_peptide.append(pep)
+
             charge.append(splitted_by_underscore[2])
-            if "(" in pep:
-                unimod = pep[pep.find("(") + 1:pep.find(")")]
-                sequence = pep[0:pep.find("(")] + pep[pep.find(")") + 1:]
-            else:
-                unimod = ""
+
+            start_list = self.find_char(pep, "(")
+            end_list = self.find_char(pep, ")")
+            #print pep, start_list, end_list
+            if len(start_list) == 0:
                 sequence = pep
+                peptide_plus_amu = pep
+            else:
+                sequence = pep[0:start_list[0]]
+                peptide_plus_amu = pep[0:start_list[0]]
+                start, end = (start_list[0], end_list[0])
+                unimod = pep[start + 1:end]
+                amu_increase = self.find_amu_increase(unimod)
+                peptide_plus_amu += "(" + str(amu_increase) + ")"
+
+                for i in range(1, len(start_list)):
+                    start, end = (start_list[i], end_list[i])
+                    unimod = pep[start + 1:end]
+                    amu_increase = self.find_amu_increase(unimod)
+
+                    sequence += pep[end_list[i - 1] + 1: start_list[i]]
+                    peptide_plus_amu += pep[end_list[i - 1] + 1: start_list[i]]
+                    peptide_plus_amu += "(" + str(amu_increase) + ")"
+                sequence += pep[end_list[-1] + 1:]
+                peptide_plus_amu += pep[end_list[-1] + 1:]
+            # if "(" in pep:
+            #     unimod = pep[pep.find("(") + 1:pep.find(")")]
+            #     sequence = pep[0:pep.find("(")] + pep[pep.find(")") + 1:]
+            # else:
+            #     unimod = ""
+            #     sequence = pep
 
             peptide_sequence.append(sequence)
 
             # Find amu increase
-            if unimod != "":
-                amu_increase = self.find_amu_increase(unimod)
-                peptide_plus_amu = pep[0:pep.find("(")] + "(" + str(amu_increase) + ")" + pep[pep.find(")") + 1:]
-            else:
-                peptide_plus_amu = pep
+            # if unimod != "":
+            #     amu_increase = self.find_amu_increase(unimod)
+            #     peptide_plus_amu = pep[0:pep.find("(")] + "(" + str(amu_increase) + ")" + pep[pep.find(")") + 1:]
+            # else:
+            #     peptide_plus_amu = pep
             amu_peptide.append(peptide_plus_amu)
-
-
 
         self._input_data['Unimod_peptide'] = unimod_peptide
         self._input_data['Amu_peptide'] = amu_peptide
@@ -130,14 +166,11 @@ class Reformat(object):
 
         unimod_access = int(unimod.split(":")[1])
 
-        unimod_loc = self._uniModdata[self._uniModdata['UniMod: Accession #'] == unimod_access].index.tolist()
+        unimod_loc = self._unimod_data[self._unimod_data['UniMod: Accession #'] == unimod_access].index.tolist()
 
-
-        amu_mass = float(self._uniModdata['UniMod: Monoisotopic Mass (not from UniMod when red)'][unimod_loc[0]])
-
+        amu_mass = float(self._unimod_data['UniMod: Monoisotopic Mass (not from UniMod when red)'][unimod_loc[0]])
 
         return amu_mass
-
 
     def reformat_protein_column(self):
         protein_num = []
@@ -148,7 +181,6 @@ class Reformat(object):
             splitted_by_pipe = str(protein).split("|")
             prot_num = splitted_by_pipe[0].split("/")[0]
             protein_num.append(prot_num)
-
             uniProtKB_str = ""
             gene_organism_str = ""
             for iter in range(1, len(splitted_by_pipe),2):
@@ -166,9 +198,10 @@ class Reformat(object):
 
         return
 
-
     def write_output_file(self):
-        self._input_data.to_csv('output/outputmatrix_OpenSWATH.csv', sep=',')
+
+
+        self._input_data.to_csv(self.output_address, sep=',')
 
 
 
